21cafec436b8fce55fa097ac06abace60915fa94
Shingyx <shingyx.dev@gmail.com>
2024-06-22 11:24:13 +1000

    Plain notification

--- app/src/main/java/com/github/shingyx/stayawake/StayAwakeService.java
+++ app/src/main/java/com/github/shingyx/stayawake/StayAwakeService.java
@@ -1,5 +1,8 @@
 package com.github.shingyx.stayawake;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.net.Uri;
 import android.provider.Settings;
@@ -9,6 +12,8 @@ import android.widget.Toast;
 
 public class StayAwakeService extends TileService {
     public final static String ACTION_TOGGLE_STAY_AWAKE = "com.github.shingyx.stayawake.TOGGLE_STAY_AWAKE";
+    public final static String ACTION_STOP_KEEPING_SCREEN_ON = "com.github.shingyx.stayawake.STOP_KEEPING_SCREEN_ON";
+    public final static int NOTIFICATION_ID = 1;
 
     private AppPreferences appPreferences;
 
@@ -23,6 +28,8 @@ public class StayAwakeService extends TileService {
         String action = intent == null ? null : intent.getAction();
         if (ACTION_TOGGLE_STAY_AWAKE.equals(action)) {
             toggleKeepingScreenOn();
+        } else if (ACTION_STOP_KEEPING_SCREEN_ON.equals(action)) {
+            stopKeepingScreenOn();
         } else {
             refreshQsTile();
         }
@@ -58,15 +65,22 @@ public class StayAwakeService extends TileService {
     }
 
     private void startKeepingScreenOn() {
-        int screenTimeout = Settings.System.getInt(getContentResolver(), Settings.System.SCREEN_OFF_TIMEOUT, Integer.MIN_VALUE);
-        appPreferences.setPreviousScreenTimeout(screenTimeout);
-        Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_OFF_TIMEOUT, Integer.MAX_VALUE);
+        if (!appPreferences.hasPreviousScreenTimeout()) {
+            int screenTimeout = Settings.System.getInt(getContentResolver(), Settings.System.SCREEN_OFF_TIMEOUT, Integer.MIN_VALUE);
+            appPreferences.setPreviousScreenTimeout(screenTimeout);
+            Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_OFF_TIMEOUT, Integer.MAX_VALUE);
+
+            createNotification();
+        }
     }
 
     private void stopKeepingScreenOn() {
-        int previousScreenTimeout = appPreferences.getPreviousScreenTimeout();
-        Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_OFF_TIMEOUT, previousScreenTimeout);
-        appPreferences.clearPreviousScreenTimeout();
+        if (appPreferences.hasPreviousScreenTimeout()) {
+            Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_OFF_TIMEOUT, appPreferences.getPreviousScreenTimeout());
+            appPreferences.clearPreviousScreenTimeout();
+
+            clearNotification();
+        }
     }
 
     private void refreshQsTile() {
@@ -78,5 +92,23 @@ public class StayAwakeService extends TileService {
             qsTile.setState(state);
             qsTile.updateTile();
         }
+    }
+
+    private void createNotification() {
+        Intent stopIntent = new Intent(ACTION_STOP_KEEPING_SCREEN_ON, null, this, getClass());
+        Notification notification = new Notification.Builder(this, StayAwakeApplication.SERVICE_NOTIFICATION_CHANNEL_ID)
+                .setContentTitle(getString(R.string.notification_title))
+                .setContentText(getString(R.string.notification_content))
+                .setSmallIcon(R.drawable.ic_stat_name)
+                .setContentIntent(PendingIntent.getService(this, 0, stopIntent, PendingIntent.FLAG_IMMUTABLE))
+                .build();
+
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.notify(NOTIFICATION_ID, notification);
+    }
+
+    private void clearNotification() {
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.cancel(NOTIFICATION_ID);
     }
 }
